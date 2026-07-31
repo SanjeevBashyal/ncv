5e6b7fbdead88bb919479d500c88689720e9ba0a
Sanjeev Bashyal <sanjeev.bashyal@ufz.de>
2026-08-01 01:39:06 +0200

    added matrix ui

--- convert-ui-to-py.sh
+++ convert-ui-to-py.sh
@@ -5,4 +5,5 @@ pyuic5 -x ncv/ui/main_window.ui -o ncv/pyui/ui_main_window.py
 pyuic5 -x ncv/ui/scatter_panel.ui -o ncv/pyui/ui_scatter_panel.py
 pyuic5 -x ncv/ui/contour_panel.ui -o ncv/pyui/ui_contour_panel.py
 pyuic5 -x ncv/ui/map_panel.ui -o ncv/pyui/ui_map_panel.py
+pyuic5 -x ncv/ui/matrix_panel.ui -o ncv/pyui/ui_matrix_panel.py
 pyuic5 -x ncv/ui/map_unavailable.ui -o ncv/pyui/ui_map_unavailable.py
